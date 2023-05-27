8b9ab98ba19627257f4187123041b1a9b6a4db46
scorcuera <scorcuera6@gmail.com>
2023-05-27 18:00:11 +0200

    created getAllSentClaps method

--- egon-server/src/controllers/claps.controller.ts
+++ egon-server/src/controllers/claps.controller.ts
@@ -10,3 +10,15 @@ export async function getAllClaps(req: Request, res: Response): Promise<Response
         console.log(e);
     }
 }
+
+export async function getAllSentClaps (req: Request, res: Response): Promise<Response | void> {
+    try {       
+        let senderId = req.params.id;
+        let connection = await connect();
+        let claps = await connection.query(`SELECT * FROM claps WHERE FromUserId = ${senderId}`)
+        return res.json(claps[0]);
+    } catch (e) {
+        console.log(e);
+    }
+   
+}
--- egon-server/src/routes/claps.routes.ts
+++ egon-server/src/routes/claps.routes.ts
@@ -1,8 +1,10 @@
 import { Router } from "express";
 import { getAllClaps } from "../controllers/claps.controller";
+import { getAllSentClaps } from "../controllers/claps.controller";
 
 let router = Router();
 
 router.route("/").get(getAllClaps);
+router.route("/:id").get(getAllSentClaps);
 
 export default router;
