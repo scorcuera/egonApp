7d946a41db900b2768d23954bedfc602bba9f22a
scorcuera <scorcuera6@gmail.com>
2023-05-27 11:52:55 +0200

    created env variables for db connection

---
+++ egon-server/database.ts
@@ -0,0 +1,12 @@
+import { createPool, Pool } from 'mysql2/promise';
+
+export async function connect(): Promise<Pool> {
+    let connection = await createPool({
+        host: process.env.DB_HOST,
+        user: process.env.DB_USER,
+        password: process.env.DB_PASSWORD,
+        database: process.env.DB_NAME,
+        connectionLimit: 10
+    })
+    return connection;
+}
