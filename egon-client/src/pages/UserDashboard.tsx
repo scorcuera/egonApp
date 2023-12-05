2e2c1effc60e9cbc6fa9136f21ecf83ce2be1050
scorcuera <scorcuera6@gmail.com>
2023-12-05 20:29:24 +0100

    Add styles to UserDashboard component

--- egon-client/src/pages/UserDashboard.tsx
+++ egon-client/src/pages/UserDashboard.tsx
@@ -1,6 +1,7 @@
 import { useContext } from "react"
 import { AuthContext } from "../contexts/auth.context"
 import clapService from "../services/claps.service";
+import "./UserDashboard.css";
 
 const UserDashboard = () => {
   const { user } = useContext(AuthContext);
@@ -8,11 +9,16 @@ const UserDashboard = () => {
   return (
     <>
       {user && (
-        <>
-          <p>Hello {user.userName}</p>
-          <p>Available claps: {user.clapsAvailable}</p>
-          <button onClick={() => clapService.getAllReceivedClaps(user.userId)}>See my claps</button>
-        </>
+        <section className="dashboard__container">
+          <div className="dashboard__info">
+            <p className="dashboard__info__greeting">Hello {user.userName} 👋</p>
+            <p className="dashboard__info__claps">Available claps: {user.clapsAvailable}</p>
+          </div>
+          <div className="dashboard__container__buttons">
+            <button className="dashboard__button" onClick={() => clapService.getAllReceivedClaps(user.userId)}>See my claps</button>
+          <button className="dashboard__button dashboard__button--primary">Send claps</button>
+          </div>
+        </section>
       )}
     </>
   )
