aaa5388ac8f060fea5621ed63fa791fd4cd52bf2
scorcuera <scorcuera6@gmail.com>
2024-02-01 20:10:12 +0100

    Create basic table template in ClapsBoard component

--- egon-client/src/components/ClapsBoard.tsx
+++ egon-client/src/components/ClapsBoard.tsx
@@ -1,8 +1,29 @@
-const ClapsBoard = ({claps}) => {
-    
+const ClapsBoard = ({ claps }) => {
   return (
     <div>
-      
+      <table>
+        <tr>
+          <th>Claps</th>
+          <th>Who</th>
+          <th>Message</th>
+          <th>Date</th>
+        </tr>
+        <tr>
+          <td>{claps.ClapCount}</td>
+        </tr>
+        <tr>
+          <td>{claps.FromUserId}</td>
+        </tr>
+        <tr>
+          <td>{claps.FromUserId}</td>
+        </tr>
+        <tr>
+          <td>{claps.Message}</td>
+        </tr>
+        <tr>
+          <td>{claps.SentAt}</td>
+        </tr>
+      </table>
     </div>
   )
 }
