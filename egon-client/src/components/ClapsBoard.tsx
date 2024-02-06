065285eb735aaf3f361f312aae61151606d05270
scorcuera <scorcuera6@gmail.com>
2024-02-06 20:00:16 +0100

    Add framer-motion animation to ClapsBoard component

--- egon-client/src/components/ClapsBoard.tsx
+++ egon-client/src/components/ClapsBoard.tsx
@@ -7,33 +7,36 @@ import {
   Td,
   TableContainer,
 } from '@chakra-ui/react'
-
+import { LazyMotion, domAnimation, m } from "framer-motion"
 
 const ClapsBoard = ({ claps }) => {
   return (
-    <TableContainer>
-      <Table variant='simple'>
-        <Thead>
-          <Tr>
-            <Th isNumeric>Claps</Th>
-            <Th>Giver</Th>
-            <Th>Message</Th>
-            <Th>Date</Th>
-          </Tr>
-        </Thead>
-        <Tbody>
-          {claps.map((clap) => {
-            return (<Tr>
-              <Td isNumeric>{clap.ClapCount}</Td>
-              <Td>{clap.FromUserId}</Td>
-              <Td>{clap.Message}</Td>
-              <Td>{clap.Date}</Td>
-            </Tr>
-            )
-          })}
-        </Tbody>
-      </Table>
-    </TableContainer>
+    <LazyMotion features={domAnimation}>
+        <TableContainer initial={{ opacity: 0 }} animate={{ opacity: 1 }} exit={{ opacity: 0 }} as={m.div}>
+          <Table variant='simple'>
+            <Thead>
+              <Tr>
+                <Th isNumeric>Claps</Th>
+                <Th>Giver</Th>
+                <Th>Message</Th>
+                <Th>Date</Th>
+              </Tr>
+            </Thead>
+            <Tbody>
+              {claps.map((clap) => {
+                return (
+                  <Tr>
+                    <Td isNumeric>{clap.ClapCount}</Td>
+                    <Td>{clap.FromUserId}</Td>
+                    <Td>{clap.Message}</Td>
+                    <Td>{clap.Date}</Td>
+                  </Tr>
+                )
+              })}
+            </Tbody>
+          </Table>
+        </TableContainer>
+    </LazyMotion>
   )
 }
 
