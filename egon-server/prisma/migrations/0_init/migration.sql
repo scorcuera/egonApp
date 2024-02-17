b8a7febc206b7cf363441775442e9d7d4bc6ab96
scorcuera <scorcuera6@gmail.com>
2024-02-17 19:26:10 +0100

    Add initial database schema and migration files

---
+++ egon-server/prisma/migrations/0_init/migration.sql
@@ -0,0 +1,46 @@
+-- CreateTable
+CREATE TABLE `claps` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `from_user_id` INTEGER NOT NULL,
+    `to_user_id` INTEGER NOT NULL,
+    `num_claps` INTEGER NOT NULL,
+    `message` TEXT NULL,
+    `sent_at` TIMESTAMP(0) NULL DEFAULT CURRENT_TIMESTAMP(0),
+
+    INDEX `from_user_id`(`from_user_id`),
+    INDEX `to_user_id`(`to_user_id`),
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `roles` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `role` VARCHAR(20) NULL,
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `users` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `name` VARCHAR(30) NOT NULL,
+    `email` VARCHAR(50) NOT NULL,
+    `password` VARCHAR(32) NOT NULL,
+    `role_id` INTEGER NOT NULL,
+    `claps_available` INTEGER NULL DEFAULT 100,
+    `created_at` TIMESTAMP(0) NULL DEFAULT CURRENT_TIMESTAMP(0),
+
+    UNIQUE INDEX `email`(`email`),
+    INDEX `role_id`(`role_id`),
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- AddForeignKey
+ALTER TABLE `claps` ADD CONSTRAINT `claps_ibfk_1` FOREIGN KEY (`from_user_id`) REFERENCES `users`(`id`) ON DELETE NO ACTION ON UPDATE NO ACTION;
+
+-- AddForeignKey
+ALTER TABLE `claps` ADD CONSTRAINT `claps_ibfk_2` FOREIGN KEY (`to_user_id`) REFERENCES `users`(`id`) ON DELETE NO ACTION ON UPDATE NO ACTION;
+
+-- AddForeignKey
+ALTER TABLE `users` ADD CONSTRAINT `users_ibfk_1` FOREIGN KEY (`role_id`) REFERENCES `roles`(`id`) ON DELETE NO ACTION ON UPDATE NO ACTION;
+
