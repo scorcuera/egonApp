85f2d13cf63f03569888bf1a98b98220138c4ad6
scorcuera <scorcuera6@gmail.com>
2024-02-17 20:30:14 +0100

    Refactor user controller to use Prisma ORM

--- egon-server/src/controllers/user.controller.ts
+++ egon-server/src/controllers/user.controller.ts
@@ -1,9 +1,9 @@
 import { Request, Response } from "express";
-import { UserModel } from "../models/user.model";
+import prisma from "../connection/client";
 
 export async function getAllUsers(req: Request, res: Response) {
     try {
-        const users = await UserModel.findAll({attributes: ['UserId', 'Username', 'UserRole', 'UserEmail']});
+        const users = await prisma.users.findMany();
         return res.json(users);
     } catch (e) {
         console.log(e);
