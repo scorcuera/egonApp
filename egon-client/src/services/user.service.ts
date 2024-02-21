0a1fdcad4cb7aee16f3beab46531a556eceab9fb
scorcuera <scorcuera6@gmail.com>
2024-02-21 11:41:09 +0100

    Refactor user.service.ts to include type annotation for users

--- egon-client/src/services/user.service.ts
+++ egon-client/src/services/user.service.ts
@@ -1,8 +1,10 @@
+import { User } from "../interfaces/user.interface";
+
 const userService = {
     async getAllUsers() {
         try {
             const result = await fetch('http://localhost:3000/users');
-            const users = await result.json();
+            const users : User[] = await result.json();
             return users;
         } catch (e) {
             console.log(e);
