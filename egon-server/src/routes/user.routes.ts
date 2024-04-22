56a5201ae391f09a4717c8550150e16021731b4a
scorcuera <scorcuera6@gmail.com>
2024-04-22 21:55:33 +0200

    Add isAdmin middleware to user route

--- egon-server/src/routes/user.routes.ts
+++ egon-server/src/routes/user.routes.ts
@@ -1,8 +1,9 @@
 import Router from "express";
 import { getAllUsers } from "../controllers/user.controller";
+import { isAdmin } from "../middlewares/session.middleware";
 
 const router = Router();
 
-router.route("/").get(getAllUsers);
+router.route("/").get(isAdmin, getAllUsers);
 
 export default router;
