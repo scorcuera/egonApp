e692a796c64e94bf24f01f50e4707fc1042a0e4a
scorcuera <scorcuera6@gmail.com>
2024-04-28 14:00:55 +0200

    Add tests for authentication functionality

---
+++ egon-server/__tests__/auth.test.ts
@@ -0,0 +1,81 @@
+import request from "supertest";
+import { ServerModel } from "../src/models/server.model";
+import { prisma } from "../src/connection/prismaClient";
+
+beforeAll(async () => {
+    await prisma.roles.create({
+        data: {
+            name: "user",
+            id: 1
+        }
+    });
+    await prisma.roles.create({
+        data: {
+            name: "admin",
+            id: 2
+        }
+    });
+})
+
+describe("Register user", () => {
+    const newUser = {
+        email: "user1@f5.org",
+        name: "user1",
+        password: "pass1"
+    }
+
+    test("should return status code 201 when a user is created", async () => {
+        const server = new ServerModel();
+        const response = await request(server.app).post("/auth/register").send(newUser);
+        expect(response.status).toBe(201);
+        expect(response.body.message).toBe("New user registered successfully");
+    });
+
+    test("should return status code 409 if user already exists", async () => {
+        const server = new ServerModel();
+        const response = await request(server.app).post("/auth/register").send(newUser);
+        expect(response.status).toBe(409);
+        expect(response.body.message).toBe("User already exists");
+    });
+});
+
+describe("Login user", () => {
+    const user = {
+        email: "user1@f5.org",
+        password: "pass1"
+    }
+
+    test("should return status code 200 when user logs in", async () => {
+        const server = new ServerModel();
+        const response = await request(server.app).post("/auth/login").send(user);
+        expect(response.status).toBe(200);
+        expect(response.body.message).toBe("User logged in");
+    });
+
+    test("should return status code 400 if user does not exist", async () => {
+        const server = new ServerModel();
+        const response = await request(server.app).post("/auth/login").send({...user, email: "pepito@f5.org"})
+        expect(response.status).toBe(400);
+        expect(response.body.message).toBe("User does not exist");
+    });
+    
+    test("should return status code 400 if password is invalid", async () => {
+        const server = new ServerModel();
+        const response = await request(server.app).post("/auth/login").send({...user, password: "pepito"})
+        expect(response.status).toBe(400);
+        expect(response.body.message).toBe("Invalid password");
+    });
+})
+
+afterAll(async () => {
+    const deleteUsers = prisma.users.deleteMany();
+    const deleteRoles = prisma.roles.deleteMany();
+
+    await prisma.$transaction([
+        deleteUsers,
+        deleteRoles
+        ]
+    );
+
+    await prisma.$disconnect();
+})
