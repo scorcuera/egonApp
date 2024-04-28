4168a9ed91e7107acaacb4e8641ea0440e4e5443
scorcuera <scorcuera6@gmail.com>
2024-04-28 14:02:25 +0200

    Refactor authentication service and remove unused code

--- egon-server/src/services/auth.service.ts
+++ egon-server/src/services/auth.service.ts
@@ -1,51 +1,11 @@
-import { Auth } from "../interfaces/auth.interface";
 import User from "../models/user.model";
-import { generateToken, verifyToken } from "../utils/jwt.handler";
-import { NewUser } from "../interfaces/user.interface";
-import { encrypt, verify } from "../utils/password.handler";
+import { verifyToken } from "../utils/jwt.handler";
 
 type JwtPayload= {
     id: number;
     iat: number;
     exp: number;
 }
-
-async function registerNewUser(authUser: NewUser) {
-    const isRegistered = await User.getUserByEmail(authUser.email);
-    if (isRegistered) {
-        return "This user already exists";
-    }
-    const passwordHash = await encrypt(authUser.password);
-    await User.createUser({...authUser, password: passwordHash});
-    return "New user registered succesfully !"
-}
-
-async function logInUser(user: Auth) {
-    const userInfo = await User.getUserByEmail(user.email);
-
-    if (!userInfo) {
-        return "Not found user";
-    }
-    const passwordHash = userInfo.password;
-    const isCorrectPassword = await verify(user.password, passwordHash);
-
-    if (!isCorrectPassword) {
-        return "Invalid Password";
-    }
-
-    const token = await generateToken(userInfo.id);
-    const userEmail = userInfo.email;
-    const userId = userInfo.id;
-
-    const data = {
-        token,
-        userId,
-        userEmail
-    }
-
-    return data;
-}
-
 
 async function checkUserFromJwt (jwt: string) {
     const jwtPayload = await verifyToken(jwt) as JwtPayload;
@@ -65,4 +25,4 @@ async function checkUserFromJwt (jwt: string) {
     return userData;
 }
 
-export { registerNewUser, logInUser, checkUserFromJwt };
+export { checkUserFromJwt };
