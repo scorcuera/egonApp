0fac4b89cc3e18184e87dfe81aa743243c8464d3
scorcuera <scorcuera6@gmail.com>
2024-04-30 20:22:09 +0200

    Fix token retrieval in loginHandler.tsx

--- egon-client/src/handlers/loginHandler.tsx
+++ egon-client/src/handlers/loginHandler.tsx
@@ -3,8 +3,8 @@ import authService from "../services/auth.service";
 
 const loginHandler = async (authUser: AuthUser) => {
     const result = await authService.loginUser(authUser);
-    if (result.token) {
-        return result.token;
+    if (result.data.token) {
+        return result.data.token;
     }
     return undefined;
 }
