e3ba652809a3dff97543567aba3d17220de1b04a
scorcuera <scorcuera6@gmail.com>
2024-04-30 20:41:21 +0200

    Update token retrieval in claps.service.ts

--- egon-client/src/services/claps.service.ts
+++ egon-client/src/services/claps.service.ts
@@ -1,8 +1,13 @@
 import { ClapForm } from "../interfaces/clap.interface";
 
 const clapService = {
-    async getAllReceivedClaps(id: number) {
-        const response = await fetch(`http://localhost:3000/claps/receivedClaps/${id}`);
+    async getAllReceivedClaps(data: any) {
+        const {id, authToken} = data;
+        const response = await fetch(`http://localhost:3000/claps/receivedClaps/${id}`, {
+            headers: {
+                'Authorization': `Bearer ${authToken}`
+            }
+        });
         const claps = await response.json();
         return claps;
     },
